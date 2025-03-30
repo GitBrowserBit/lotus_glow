77de9f3835390c4f8b5daeabcdb169488440af5e
GitBrowserBit <browsergit@gmail.com>
2025-03-30 10:22:14 -0300

    projeto inicial: uma landpage para apenas layout

---
+++ app/components/sections/Hero.tsx
@@ -0,0 +1,11 @@
+import Header from "../utils/Header";
+
+const Hero = () => {
+    return(
+        <section>
+            <Header />
+        </section>
+    )
+}
+
+export default Hero;
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "./components/sections/Hero";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Hero />
+      </main>
+      <footer className={styles.footer}>
+        
+      </footer>
+    </div>
+  );
+}
